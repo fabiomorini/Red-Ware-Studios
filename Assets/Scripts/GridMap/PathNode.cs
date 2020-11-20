e323e38dc0d80683814117ba1c0a48428b28a91b
Antonio <antonio.robreno@enti.cat>
2020-11-20 18:24:39 +0100

    TraceLine Movimiento y Tablero por Casillas

---
+++ Assets/Scripts/GridMap/PathNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode
+{
+    private Grid<PathNode> grid;
+    public int x;
+    public int y;
+
+    public int gCost;   //Valor de G
+    public int hCost;   //Valor de H
+    public int fCost;   //Valor de F
+
+    public bool isWalkable;
+    public PathNode cameFromNode;   //Nodo del que venimos
+
+    
+    public PathNode(Grid<PathNode> grid, int x, int y)
+    {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        isWalkable = true;
+    }
+
+    public void CalculateFCost()
+    {
+        fCost = gCost + hCost;
+    }
+
+    public override string ToString()
+    {
+        return x + "," + y;
+    }
+}
