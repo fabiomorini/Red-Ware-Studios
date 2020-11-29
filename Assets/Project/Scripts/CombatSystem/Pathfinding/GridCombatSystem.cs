277cdcfe5a93a2fe2d60c7570f977fce86ba06c1
Antonio <antonio.robreno@enti.cat>
2020-11-29 19:48:33 +0100

    Pathfinding Finalizado
    Al fin

---
+++ Assets/Project/Scripts/CombatSystem/Pathfinding/GridCombatSystem.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GridPathfindingSystem;
+
+public class GridCombatSystem : MonoBehaviour {
+
+    [SerializeField] private UnitGridCombat[] unitGridCombatArray;
+
+    private State state;
+    private UnitGridCombat unitGridCombat;
+    private List<UnitGridCombat> blueTeamList;
+    private List<UnitGridCombat> redTeamList;
+    private int blueTeamActiveUnitIndex;
+    private int redTeamActiveUnitIndex;
+    private bool canMoveThisTurn;
+    private bool canAttackThisTurn;
+
+    private enum State {
+        Normal,
+        Waiting
+    }
+
+    private void Awake() {
+        state = State.Normal;
+    }
+
+    private void Start() {
+        blueTeamList = new List<UnitGridCombat>();
+        redTeamList = new List<UnitGridCombat>();
+        blueTeamActiveUnitIndex = -1;
+        redTeamActiveUnitIndex = -1;
+
+        // Set all UnitGridCombat on their GridPosition
+        foreach (UnitGridCombat unitGridCombat in unitGridCombatArray) {
+            GameHandler_GridCombatSystem.Instance.GetGrid().GetGridObject(unitGridCombat.GetPosition()).SetUnitGridCombat(unitGridCombat);
+            if (unitGridCombat.GetTeam() == UnitGridCombat.Team.Blue) {
+                blueTeamList.Add(unitGridCombat);
+            } else {
+                redTeamList.Add(unitGridCombat);
+            }
+        }
+
+        SelectNextActiveUnit();
+        UpdateValidMovePositions();
+    }
+
+    private void SelectNextActiveUnit() {
+        if (unitGridCombat == null || unitGridCombat.GetTeam() == UnitGridCombat.Team.Red) {
+            unitGridCombat = GetNextActiveUnit(UnitGridCombat.Team.Blue);
+        } else {
+            unitGridCombat = GetNextActiveUnit(UnitGridCombat.Team.Red);
+        }
+
+        GameHandler_GridCombatSystem.Instance.SetCameraFollowPosition(unitGridCombat.GetPosition());
+        canMoveThisTurn = true;
+        canAttackThisTurn = true;
+    }
+    
+    private UnitGridCombat GetNextActiveUnit(UnitGridCombat.Team team) {
+        if (team == UnitGridCombat.Team.Blue) {
+            blueTeamActiveUnitIndex = (blueTeamActiveUnitIndex + 1) % blueTeamList.Count;
+            if (blueTeamList[blueTeamActiveUnitIndex] == null ) {
+                // Unit is Dead, get next one
+                return GetNextActiveUnit(team);
+            } else {
+                return blueTeamList[blueTeamActiveUnitIndex];
+            }
+        } else {
+            redTeamActiveUnitIndex = (redTeamActiveUnitIndex + 1) % redTeamList.Count;
+            if (redTeamList[redTeamActiveUnitIndex] == null ) {
+                // Unit is Dead, get next one
+                return GetNextActiveUnit(team);
+            } else {
+                return redTeamList[redTeamActiveUnitIndex];
+            }
+        }
+    }
+
+    private void UpdateValidMovePositions() {
+        Grid<GridObject> grid = GameHandler_GridCombatSystem.Instance.GetGrid();
+        GridPathfinding gridPathfinding = GameHandler_GridCombatSystem.Instance.gridPathfinding;
+
+        // Get Unit Grid Position X, Y
+        grid.GetXY(unitGridCombat.GetPosition(), out int unitX, out int unitY);
+
+        // Set entire Tilemap to Invisible
+        GameHandler_GridCombatSystem.Instance.GetMovementTilemap().SetAllTilemapSprite(
+            MovementTilemap.TilemapObject.TilemapSprite.None
+        );
+
+        // Reset Entire Grid ValidMovePositions
+        for (int x = 0; x < grid.GetWidth(); x++) {
+            for (int y = 0; y < grid.GetHeight(); y++) {
+                grid.GetGridObject(x, y).SetIsValidMovePosition(false);
+            }
+        }
+
+        int maxMoveDistance = 5;
+        for (int x = unitX - maxMoveDistance; x <= unitX + maxMoveDistance; x++) {
+            for (int y = unitY - maxMoveDistance; y <= unitY + maxMoveDistance; y++) {
+                if (gridPathfinding.IsWalkable(x, y)) {
+                    // Position is Walkable
+                    if (gridPathfinding.HasPath(unitX, unitY, x, y)) {
+                        // There is a Path
+                        if (gridPathfinding.GetPath(unitX, unitY, x, y).Count <= maxMoveDistance) {
+                            // Path within Move Distance
+
+                            // Set Tilemap Tile to Move
+                            GameHandler_GridCombatSystem.Instance.GetMovementTilemap().SetTilemapSprite(
+                                x, y, MovementTilemap.TilemapObject.TilemapSprite.Move
+                            );
+
+                            grid.GetGridObject(x, y).SetIsValidMovePosition(true);
+                        } else { 
+                            // Path outside Move Distance!
+                        }
+                    } else {
+                        // No valid Path
+                    }
+                } else {
+                    // Position is not Walkable
+                }
+            }
+        }
+    }
+
+    private void Update() {
+        switch (state) {
+            case State.Normal:
+                if (Input.GetMouseButtonDown(0)) {
+                    Grid<GridObject> grid = GameHandler_GridCombatSystem.Instance.GetGrid();
+                    GridObject gridObject = grid.GetGridObject(GetMouseWorldPosition());
+
+                    if (gridObject.GetIsValidMovePosition()) {
+                        // Valid Move Position
+
+                        if (canMoveThisTurn) {
+                            canMoveThisTurn = false;
+
+                            state = State.Waiting;
+
+                            // Set entire Tilemap to Invisible
+                            GameHandler_GridCombatSystem.Instance.GetMovementTilemap().SetAllTilemapSprite(
+                                MovementTilemap.TilemapObject.TilemapSprite.None
+                            );
+
+                            // Remove Unit from current Grid Object
+                            grid.GetGridObject(unitGridCombat.GetPosition()).ClearUnitGridCombat();
+                            // Set Unit on target Grid Object
+                            gridObject.SetUnitGridCombat(unitGridCombat);
+
+                            unitGridCombat.MoveTo(GetMouseWorldPosition(), () => {
+                                state = State.Normal;
+                                UpdateValidMovePositions();
+                                TestTurnOver();
+                            });
+                        }
+                    }
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    ForceTurnOver();
+                }
+                break;
+            case State.Waiting:
+                break;
+        }
+    }
+
+    private void TestTurnOver() {
+        if (!canMoveThisTurn && !canAttackThisTurn) {
+            // Cannot move or attack, turn over
+            ForceTurnOver();
+        }
+    }
+
+    private void ForceTurnOver() {
+        SelectNextActiveUnit();
+        UpdateValidMovePositions();
+    }
+
+
+
+    public class GridObject {
+
+        private Grid<GridObject> grid;
+        private int x;
+        private int y;
+        private bool isValidMovePosition;
+        private UnitGridCombat unitGridCombat;
+
+        public GridObject(Grid<GridObject> grid, int x, int y) {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetIsValidMovePosition(bool set) {
+            isValidMovePosition = set;
+        }
+
+        public bool GetIsValidMovePosition() {
+            return isValidMovePosition;
+        }
+
+        public void SetUnitGridCombat(UnitGridCombat unitGridCombat) {
+            this.unitGridCombat = unitGridCombat;
+        }
+
+        public void ClearUnitGridCombat() {
+            SetUnitGridCombat(null);
+        }
+
+        public UnitGridCombat GetUnitGridCombat() {
+            return unitGridCombat;
+        }
+
+    }
+
+    // Get Mouse Position in World with Z = 0f
+        public static Vector3 GetMouseWorldPosition() {
+            Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+            vec.z = 0f;
+            return vec;
+        }
+        public static Vector3 GetMouseWorldPositionWithZ() {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera) {
+            return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+        }
+        public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera) {
+            Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+            return worldPosition;
+        }
+
+}
---
+++ Assets/Project/Scripts/CombatSystem/Pathfinding/UnitGridCombat.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitGridCombat : MonoBehaviour {
+
+    [SerializeField] private Team team;
+
+    private Character_Base characterBase;
+    private GameObject selectedGameObject;
+    private MovePositionPathfinding movePosition;
+    private State state;
+
+    public enum Team {
+        Blue,
+        Red
+    }
+
+    private enum State {
+        Normal,
+        Moving,
+        Attacking
+    }
+
+    private void Awake() {
+        characterBase = GetComponent<Character_Base>();
+        selectedGameObject = transform.Find("Selected").gameObject;
+        movePosition = GetComponent<MovePositionPathfinding>();
+        state = State.Normal;
+    }
+
+
+    private void Update() {
+        switch (state) {
+            case State.Normal:
+                break;
+            case State.Moving:
+                break;
+            case State.Attacking:
+                break;
+        }
+    }
+
+    public void SetSelectedVisible(bool visible) {
+        selectedGameObject.SetActive(visible);
+    }
+
+    public void MoveTo(Vector3 targetPosition, Action onReachedPosition) {
+        state = State.Moving;
+        movePosition.SetMovePosition(targetPosition + new Vector3(1, 1), () => {
+            state = State.Normal;
+            onReachedPosition();
+        });
+    }
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public Team GetTeam() {
+        return team;
+    }
+
+    public bool IsEnemy(UnitGridCombat unitGridCombat) {
+        return unitGridCombat.GetTeam() != team;
+    }
+
+}
