56337f18fccf227c28eadde530671d29d779e109
Fabio <Morini>
2020-12-17 21:55:11 +0100

    fix

--- Assets/Project/Scripts/characters/CHARACTER_MNG.cs
+++ Assets/Project/Scripts/characters/CHARACTER_MNG.cs
@@ -14,7 +14,7 @@ public class CHARACTER_MNG : MonoBehaviour
     private GameObject hola; // deep lore
 
     //Escenas de Unity por buildIndex
-    private int IndexL1 = 1;
+    private int IndexL1 = 2;
     private int IndexL2 = 3;
     private int IndexL3 = 4;
     private int IndexL4 = 5;
@@ -22,12 +22,12 @@ public class CHARACTER_MNG : MonoBehaviour
     private int IndexL6 = 7;
 
     //MAX personajes por nivel
-    private int maxL1 = 5;
-    private int maxL2 = 5;
-    private int maxL3 = 5;
+    private int maxL1 = 2;
+    private int maxL2 = 3;
+    private int maxL3 = 4;
     private int maxL4 = 5;
-    private int maxL5 = 5;
-    private int maxL6 = 5;
+    private int maxL5 = 6;
+    private int maxL6 = 7;
 
     private void Start()
     {
