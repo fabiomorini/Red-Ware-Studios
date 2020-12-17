93b2559a8cb01aa07fe3778315ee525916dfc960
DanielCasa <daniel.peracaula@enti.cat>
2020-12-17 21:33:10 +0100

    Mapamundi completado
    Añadidos

---
+++ Assets/Project/Scripts/Mapamundi/FightInteraction3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FightInteraction3 : MonoBehaviour
+{
+    [SerializeField]
+
+    private Text pressEText;
+    private bool pressedCombatButton;
+
+    // Use this for initialization
+    private void Start()
+    {
+        pressEText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (pressedCombatButton && Input.GetKeyDown(KeyCode.E))
+            Combat();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            pressEText.gameObject.SetActive(true);
+            pressedCombatButton = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            pressEText.gameObject.SetActive(false);
+            pressedCombatButton = false;
+        }
+    }
+
+    private void Combat()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+}
