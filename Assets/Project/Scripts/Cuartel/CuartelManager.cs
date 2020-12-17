f7b1795b17b004d5a1b0736b443a97aaac387cbf
Fabio <Morini>
2020-12-17 23:17:48 +0100

    UI acabada

---
+++ Assets/Project/Scripts/Cuartel/CuartelManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CuartelManager : MonoBehaviour
+{
+    public int coins = 0;
+    private int knightCounter = 0;
+    private int ArcherCounter = 0;
+    private int knightPrice = 100;
+    private int archerPrice = 120;
+
+    public TMP_Text coinsText;
+    public TMP_Text ArcherText;
+    public TMP_Text KnightText;
+    public GameObject cuartel;
+
+    private bool isActive;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && !isActive)
+        {
+            cuartel.SetActive(true);
+            isActive = true;
+        }
+        if(Input.GetKey("escape") && isActive)
+        {
+            cuartel.SetActive(false);
+            isActive = false;
+        }
+        coinsText.SetText("¥ " + coins);
+        ArcherText.SetText("x " + ArcherCounter);
+        KnightText.SetText("x " + knightCounter);
+    }
+
+    public void BuyKnight()
+    {
+        if(coins >= knightPrice)
+        {
+            coins -= knightPrice;
+            knightCounter++;
+        }
+
+    }
+    public void BuyArcher()
+    {
+        if (coins >= knightPrice)
+        {
+            coins -= archerPrice;
+            ArcherCounter++;
+        }
+    }
+
+}
--- Assets/Project/Scripts/Player/PlayerVariables.cs
+++ Assets/Project/Scripts/Player/PlayerVariables.cs
@@ -7,27 +7,6 @@ public class PlayerVariables : MonoBehaviour
     public int coins = 0;
     public int allies_counter = 0;
     private GridCombatSystem gridCombatSystem;
-    [HideInInspector]
-    public List<CHARACTER_PREFS> alliesList;
-    private CHARACTER_PREFS characterPrefs;
-
-
-    private void Start()
-    {
-        alliesList = new List<CHARACTER_PREFS>();
-
-        for (int i = 0; i < allies_counter; i++)
-        {
-            alliesList.Add(characterPrefs);
-        }
-    }
-
-    public void spawnAlly() {
-        // pasarle a la funcion el máximo de tropas por cada nivel
-        // comparar allies counter 
-        // for que por cada aliado spawn un prefab de ally en la escena de batalla con las estadísticas
-        // el maximo del for es el numero de aliados máximos que puedes espawnear, si en la escena puedes jugar con 10, el for llega a 10
-    }
 
     public void BuyAlly(int price)
     {
