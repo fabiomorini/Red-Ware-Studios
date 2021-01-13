b16b5f1561d2a63ba8b7e0178403376e03642ea1
Fabio <Morini>
2021-01-13 21:14:17 +0100

    banderitas minimapa

--- Assets/Project/Scripts/UI/HealthBar.cs
+++ Assets/Project/Scripts/UI/HealthBar.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
+
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    private TMP_Text maxHealth;
+    private TMP_Text currHealth;
+    private TMP_Text level;
 
     public void SetMaxHealth(float health)
     {
