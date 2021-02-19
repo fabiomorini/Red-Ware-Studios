9fd2eaab5c79be446ecb86d05f961cc48e3c94a5
DanielCasa <daniel.peracaula@enti.cat>
2021-02-19 19:43:41 +0100

    FullScreen (Mejorar Resoluciones)

---
+++ Assets/fullscreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fullscreen : MonoBehaviour
+{
+    public void FullScreen(bool is_full) 
+    {
+        Screen.fullScreen = is_full;
+
+        Debug.Log("Fullscreen is " + is_full);
+    }
+}
