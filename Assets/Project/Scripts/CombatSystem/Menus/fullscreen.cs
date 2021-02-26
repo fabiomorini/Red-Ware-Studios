2b42faf8dabb1697c949e8024ab08bb3088fc899
DanielCasa <daniel.peracaula@enti.cat>
2021-02-26 19:58:12 +0100

    Last Update

--- Assets/Project/Scripts/CombatSystem/Menus/fullscreen.cs
+++ Assets/Project/Scripts/CombatSystem/Menus/fullscreen.cs
@@ -7,6 +7,7 @@ public class fullscreen : MonoBehaviour
     public void FullScreen(bool is_full) 
     {
         Screen.fullScreen = is_full;
+        Screen.SetResolution(1920, 1080, is_full);
 
         Debug.Log("Fullscreen is " + is_full);
     }
